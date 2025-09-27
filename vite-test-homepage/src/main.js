80a4369940e25093bce13747cc73c4bc3771da09
Arthur Now <arthurnow@example.com>
2025-09-27 22:28:10 +0200

    Add vite-test-homepage project

---
+++ vite-test-homepage/src/main.js
@@ -0,0 +1,3 @@
+document.body.style.fontFamily = 'sans-serif'
+document.body.style.textAlign = 'center'
+console.log('Vite test homepage loaded!')
---
+++ vite-test-homepage/vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/WebTrials/vite-test-homepage/', // matches your repo/project path
+})
